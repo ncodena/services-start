6753e6c19584e852dda8b906ce7a1e11cb8729df
Nuria <c.odena.nuria@gmail.com>
2020-05-25 14:58:52 +0200

    creating a data service

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { LoggingService} from '../logging.service';
-import { AccountService } from '../account.service';
+import { AccountsService } from '../account.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService, AccountService]
+  providers: [LoggingService, AccountsService]
 })
 export class NewAccountComponent {
   
-  constructor(private loggingService: LoggingService, private accountService: AccountService) {}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService) {}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountService.addAccount(accountName, accountStatus)
+    this.accountsService.addAccount(accountName, accountStatus)
 
     this.loggingService.logStatusChange(accountStatus);
   }
